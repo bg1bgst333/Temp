41ec0d344ac907494c35983666a20c4b6139888c
miura <h.miura@freebit.net>
2017-10-31 11:31:46 +0900

    -メッセージボックスで"Hello, Aoi32!"の表示.

---
+++ Aoi32/Aoi32/WinMain.cpp
@@ -0,0 +1,15 @@
+// ヘッダのインクルード
+// 既定のヘッダ
+#include <tchar.h>		// TCHAR型
+#include <windows.h>	// 標準WindowsAPI
+
+// _tWinMain関数の定義
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nShowCmd) {
+
+	// "Hello, Aoi32!"と表示.
+	MessageBox(NULL, _T("Hello, Aoi32!"), _T("Aoi32"), MB_OK);	// MessageBoxでHello, Aoi32!"と表示.
+
+	// プログラムの終了
+	return 0;	// 0を返して正常終了.
+
+}
